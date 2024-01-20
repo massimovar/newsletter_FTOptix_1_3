b0c3f2516af50df03442a9ee560e27ab239e0b85
MasVar <m.variolo@asem.it>
2024-01-20 15:51:56 +0100

    Fixed time range CRUD management

--- ProjectFiles/NetSolution/RangesManager.cs
+++ ProjectFiles/NetSolution/RangesManager.cs
@@ -20,6 +20,7 @@ using FTOptix.CommunicationDriver;
 using FTOptix.MicroController;
 using FTOptix.CODESYS;
 using static System.Formats.Asn1.AsnWriter;
+using System.Collections.Generic;
 #endregion
 
 public class RangesManager : BaseNetLogic
@@ -29,6 +30,7 @@ public class RangesManager : BaseNetLogic
     private IUANode _trendPens;
     private IUANode _ranges;
     private IUANode _rangesContainer;
+
     private ReferencesObserver _referencesObserver;
     private IEventRegistration referencesEventRegistration;
 
@@ -52,18 +54,43 @@ public class RangesManager : BaseNetLogic
 
     private class ReferencesObserver : IReferenceObserver
     {
-        private IUANode uiContainer;
-        private Store store;
-        private IUANode pens;
-        private IUANode rangesNode;
+        private IUANode _uiContainer;
+        private Store _store;
+        private IUANode _pens;
+        private IUANode _rangesNode;
+        private List<IUANode> _trendTimeRanges;
 
         public ReferencesObserver(IUANode rangesNode, IUANode pens, IUANode uiContainer, Store store)
         {
-            this.uiContainer = uiContainer;
-            this.store = store;
-            this.pens = pens;
-            this.rangesNode = rangesNode;
+            this._uiContainer = uiContainer;
+            this._store = store;
+            this._pens = pens;
+            this._rangesNode = rangesNode;
+            _trendTimeRanges = new List<IUANode>();
+
             rangesNode.Children.ToList().ForEach(CreateRangeUI);
+
+        }
+
+        public void OnReferenceAdded(IUANode sourceNode, IUANode targetNode, NodeId referenceTypeId, ulong senderId)
+        {
+            AddTimeRange(targetNode);
+            CreateRangesUI();
+        }
+
+        public void OnReferenceRemoved(IUANode sourceNode, IUANode targetNode, NodeId referenceTypeId, ulong senderId)
+        {
+            RemoveTimeRange(targetNode);
+            CreateRangesUI();
+        }
+
+        private void CreateRangesUI()
+        {
+            _uiContainer.Children.Clear();
+            foreach (var tr in _trendTimeRanges)
+            {
+                CreateRangeUI(tr);
+            }
         }
 
         private void CreateRangeUI(IUANode rangeNode)
@@ -71,23 +98,40 @@ public class RangesManager : BaseNetLogic
             TimeRange range = (TimeRange)(rangeNode as IUAVariable).Value.Value;
             var startTime = range.StartTime;
             var endTime = range.EndTime;
-            var trendWidgetInstance = InformationModel.Make<TrendRangeWidget>(rangeNode.BrowseName + rangeNode.NodeId);
+            var trendWidgetInstance = InformationModel.Make<TrendRangeWidget>(rangeNode.BrowseName);
             trendWidgetInstance.GetVariable("RangeStartDate").Value = startTime;
             trendWidgetInstance.GetVariable("RangeEndDate").Value = endTime;
             var timeSpan = range.EndTime - range.StartTime;
             trendWidgetInstance.GetVariable("Timespan").Value = timeSpan.TotalMilliseconds;
-            uiContainer.Add(trendWidgetInstance);
+            _uiContainer.Add(trendWidgetInstance);
         }
 
-        public void OnReferenceAdded(IUANode sourceNode, IUANode targetNode, NodeId referenceTypeId, ulong senderId)
+        private void AddTimeRange(IUANode targetNode)
         {
-            CreateRangeUI(targetNode);
+            _trendTimeRanges.Add(targetNode);
+            SortTimeRanges();
         }
 
-        public void OnReferenceRemoved(IUANode sourceNode, IUANode targetNode, NodeId referenceTypeId, ulong senderId)
+        private void RemoveTimeRange(IUANode targetNode)
         {
-            var uiRange = uiContainer.Children.Get(targetNode.BrowseName + targetNode.NodeId);
-            uiRange?.Delete();
+            var trToRemove = ((FTOptix.Core.TimeRange)((UAManagedCore.UAVariable)targetNode).Value.Value);
+            var tr = _trendTimeRanges.FirstOrDefault(tr =>
+                ((FTOptix.Core.TimeRange)((UAManagedCore.UAVariable)tr).Value.Value).StartTime == trToRemove.StartTime
+                &&
+                ((FTOptix.Core.TimeRange)((UAManagedCore.UAVariable)tr).Value.Value).EndTime == trToRemove.EndTime);
+
+            _trendTimeRanges.Remove(tr);
+            SortTimeRanges();
+        }
+
+        private void SortTimeRanges()
+        {
+            _trendTimeRanges.Sort(
+                (tr1, tr2) =>
+                    DateTime.Compare(
+                        ((FTOptix.Core.TimeRange)((UAManagedCore.UAVariable)tr1).Value.Value).StartTime,
+                        ((FTOptix.Core.TimeRange)((UAManagedCore.UAVariable)tr2).Value.Value).StartTime
+                ));
         }
     }
 }
